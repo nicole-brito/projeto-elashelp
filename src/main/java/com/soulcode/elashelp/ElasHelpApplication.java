4e675ed49e495231e081640d776fc8470007ef9a
nic <nicolem.britto@gmail.com>
2024-05-02 21:50:09 -0300

    🌱 Initial Commit

---
+++ src/main/java/com/soulcode/elashelp/ElasHelpApplication.java
@@ -0,0 +1,13 @@
+package com.soulcode.elashelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasHelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soulcode/elashelp/ElasHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulcode.elashelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
