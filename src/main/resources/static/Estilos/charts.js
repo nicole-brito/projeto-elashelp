2e23153ae904bed46695f445637ed055e0e9370f
nic <nicolem.britto@gmail.com>
2024-05-08 19:54:16 -0300

    Add charts.js

---
+++ src/main/resources/static/Estilos/charts.js
@@ -0,0 +1,117 @@
+
+<!--    Mostra o total de tickets e também os que foram finalizados -->
+fetch('/ticket/total-by-sector')
+    .then(response => response.json())
+    .then(totalData => {
+        fetch('/ticket/finished-by-sector')
+            .then(response => response.json())
+            .then(finishedData => {
+                var xValues = Object.keys(totalData); // Nomes dos setores
+                var totalValues = Object.values(totalData); // Quantidade total de tickets
+                var finishedValues = xValues.map(x => finishedData[x] || 0); // Quantidade de tickets finalizados
+
+                new Chart("ticketsbysetorestatus", {
+                    type: "bar",
+                    data: {
+                        labels: xValues,
+                        datasets: [{
+                            label: 'Total Tickets',
+                            data: totalValues,
+                            backgroundColor: 'rgba(0, 123, 255, 0.5)',
+                            borderColor: 'rgba(0, 123, 255, 1)',
+                            borderWidth: 1
+                        }, {
+                            label: 'Finished Tickets',
+                            data: finishedValues,
+                            type: 'line',
+                            fill: false,
+                            borderColor: 'rgba(40, 167, 69, 1)'
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        title: {
+                            display: true,
+                            text: 'Total de Tickets e Tickets Finalizados por Setor'
+                        },
+                        tooltips: {
+                            mode: 'index',
+                            intersect: false
+                        },
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
+            });
+    });
+
+<!--    Mostra os tickets abertos ordenados por setor-->
+fetch('/ticket/open-by-sector')
+    .then(response => response.json())
+    .then(data => {
+        var xValues = Object.keys(data); // Nomes dos setores
+        var yValues = Object.values(data); // Quantidade de tickets abertos
+
+        var barColors = [
+            "#d55a7c",
+            "#00aba9",
+            "#2b5797",
+            "#e8c3b9",
+            "#1e7145"
+        ];
+
+        new Chart("ticketsbysetor", {
+            type: "doughnut",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: barColors,
+                    data: yValues
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: "Tickets Abertos por Setor"
+                }
+            }
+        });
+    });
+
+// Mostra os tickets por status
+fetch('/ticket/by-status')
+    .then(response => response.json())
+    .then(data => {
+        var labels = Object.keys(data); // Status
+        var values = Object.values(data); // Quantidade de tickets
+
+        var colors = [
+            "#d55a7c",
+            "#00aba9",
+            "#2b5797",
+            "#e8c3b9",
+            "#1e7145"
+        ];
+
+        new Chart("ticketsByStatus", {
+            type: "pie",
+            data: {
+                labels: labels,
+                datasets: [{
+                    backgroundColor: colors,
+                    data: values
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Tickets por Status'
+                }
+            }
+        });
+    });
