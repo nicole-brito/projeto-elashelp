101f0a4272042a793a8c628d0eea76e500ee9e4c
nic <nicolem.britto@gmail.com>
2024-05-10 11:18:09 -0300

    Separação REST e MVC do Técnico

--- src/main/java/com/soulcode/elashelp/Services/TecnicoService.java
+++ src/main/java/com/soulcode/elashelp/Services/TecnicoService.java
@@ -14,10 +14,12 @@ import java.util.List;
 
 @Service
 public class TecnicoService {
+
     @Autowired
     private TecnicoRepository tecnicoRepository;
+
     @Autowired
-    private  LoginRepository loginRepository;
+    private LoginRepository loginRepository;
 
     public List<Tecnico> findAll() {
         return tecnicoRepository.findAll();
@@ -27,7 +29,7 @@ public class TecnicoService {
 //        if (tecnicoRepository.existsById(tecnico.getMatricula())) {
 //            throw new RuntimeException("Já existe um Técnico com esse cadastro");
 //        }
-       tecnico = tecnicoRepository.save(tecnico);
+        tecnico = tecnicoRepository.save(tecnico);
 
         Login login = new Login();
         login.setEmail(tecnico.getEmail());
@@ -52,7 +54,5 @@ public class TecnicoService {
     public String deleteById(Long matricula) {
         tecnicoRepository.deleteById(matricula);
         return "Técnico excluído com sucesso! Matrícula: " + matricula;
-    }git pu
-
-
+    }
 }
